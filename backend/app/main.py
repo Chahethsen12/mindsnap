64d1fb8bc7e4da4307e2c336bed722025a5206b3
Chaheth Senevirathne <chahethsen12@gmail.com>
2026-03-10 15:11:52 +0530

    Fix: add Vercel URL to CORS allowed origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 # Allow React frontend to communicate with this backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://mindsnap-sigma.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
